615292e55b252721e6f8b7818c096c5eee875f54
ohadApiiro <ohad@apiiro.com>
2023-10-26 16:34:31 +0300

    a

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+from foo import Foo
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
+    f = Foo()
+    f.bar()
     return jsonify({'message': 'Hello, this is a simple Flask API!'})
 
 if __name__ == '__main__':
